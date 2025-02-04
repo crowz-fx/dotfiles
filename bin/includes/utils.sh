e215d0f75ce7a547fe2dec6a67474b2a3cbc4109
Lui Crowie <22945441+crowz-fx@users.noreply.github.com>
2025-02-04 17:58:44 +0000

    tweaks for mimes, notif styling, fix for all scripts and reshuffled some of the includes and helper functions

--- bin/includes/utils.sh
+++ bin/includes/utils.sh
@@ -1,12 +1,30 @@
-# collection of utility functions at the disposal of all the bin/ scripts
+# collection of utility functions at the disposal of all 
+# the bin/ scripts
+##################################################################
+# raw functions below
 
 send_notif() {
-  notify-send -t 2000 "$@"
+  notify-send -t 5000 "$@"
 }
 
-check_positional_arg() {
-  if [ -z "$1" ]; then
-    notif "Didn't supply [$2]"
+send_notif_critical() {
+  notify-send -u critical -t 10000 "$@"
+}
+
+##################################################################
+# script_ is used in scripts
+script_notif() {
+  send_notif "$script_title" "$@"
+}
+
+script_notif_critical() {
+  send_notif_critical "$script_title" "$@"
+}
+
+script_check_positional_arg() {
+  if [ -z "$1" ] || [[ "$1" == "" ]] || [[ "$1" == "null" ]]; then
+    echo "Didn't supply [$2]"
+    script_notif_critical "Didn't supply [$2]"
     exit 1
   fi
 }
