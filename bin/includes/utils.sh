e6f73d8dcfcc477dd681a549b15ff8299ed355e5
Lui Crowie <22945441+crowz-fx@users.noreply.github.com>
2025-02-10 13:32:32 +0000

    added in handling of agent keys, colours and echo wrapping for use in scripts etc.

--- bin/includes/utils.sh
+++ bin/includes/utils.sh
@@ -1,14 +1,32 @@
 # collection of utility functions at the disposal of all 
 # the bin/ scripts
 ##################################################################
-# raw functions below
+# sauce
+. ~/.config/bin/includes/colours.sh
 
+# raw functions below, used anywhere
 send_notif() {
   notify-send -t 5000 "$@"
 }
 
 send_notif_critical() {
   notify-send -u critical -t 10000 "$@"
+}
+
+echod() {
+  echo -e "${BLUE_COLOUR}D |${RESET_COLOUR} ${@}"
+}
+
+echoi() {
+  echo -e "${GREEN_COLOUR}I |${RESET_COLOUR} ${@}"
+}
+
+echow() {
+  echo -e "${YELLOW_COLOUR}W |${RESET_COLOUR} ${@}"
+}
+
+echoe(){
+  echo -e "${RED_COLOUR}E | ${RESET_COLOUR} ${@}"
 }
 
 ##################################################################
